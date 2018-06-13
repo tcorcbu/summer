cf3a797b2465216f429c37a3f13fdabfe9b6fa5c
tcorcbu <tcorc@bu.edu>
2018-06-13 13:24:36 -0400

    Create example.js

---
+++ example.js
@@ -0,0 +1,59 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
+  
+  
+  $scope.dateIdeas = [
+      {
+          id: 0,
+          title: "Rent a sailboat on the Charles",
+          description: "Rent a sailboat in the late afternoon / evening and sail around during sunset on the river",
+          link: "https://www.community-boating.org/rentals/sailboat-rentals/",
+          imageUrl: "https://s3.amazonaws.com/boatbound_production/city_template_photos/city_photos/000/000/053/scaled_down_1440/charles_river_2.jpg",
+          date: "",
+          status: ""
+      }, 
+      {
+          id: 1,
+          title: "Boston Harbor Sunset Cruise",
+          description: "Take a tour of Boston Harbor during sunset",
+          link: "https://www.bostonharborcruises.com/harbor-cruises/sunset-cruise/",
+          imageUrl: "https://www.bostonharborcruises.com/media/5574/istock-611215172.jpg",
+          date: "",
+          status: ""
+      },
+      {
+          id: 2,
+          title: "Boston Harbor Islands Cruise",
+          description: "Take a ferry over and explore one of the islands in Boston Harbor",
+          link: "https://www.bostonharborcruises.com/harbor-islands/",
+          imageUrl: "https://www.bostonharborcruises.com/media/1073/spectacleisland_header.jpg",
+          date: "",
+          status: ""
+      },
+      {
+          id: 3,
+          title: "Revere Hotel Rooftop Pool",
+          description: "Take a ferry over and explore one of the islands in Boston Harbor",
+          link: "https://www.reverehotel.com/eat-drink/rooftop-revere",
+          imageUrl: "https://cdn10.bostonmagazine.com/wp-content/uploads/2013/05/RTP.jpg",
+          date: "",
+          status: ""
+      },
+      {
+          id: 4,
+          title: "Walk Through Boston Public Garden",
+          description: "Take a ferry over and explore one of the islands in Boston Harbor",
+          link: "",
+          imageUrl: "",
+          date: "",
+          status: ""
+      }
+    ];
+  
+  
+  $scope.myInterval = 7000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  
+
+});
